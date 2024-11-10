ee39ef7ea1ebd91e14c4934f42cd823d57467198
Németh Csaba Tibor <nemeth.csaba.tibor@gmail.com>
2024-11-10 10:06:43 +0100

    Fix rule scope

--- app/Models/Rule.php
+++ app/Models/Rule.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 use App\Enums\Direction;
 use App\Enums\RuleType;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Schema\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
