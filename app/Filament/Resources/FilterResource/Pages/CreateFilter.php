b4bc901de8202c23de3ed300a13a5b3400a0a1c1
Németh Csaba Tibor <nemeth.csaba.tibor@gmail.com>
2024-11-17 20:57:46 +0100

    Updated filter

--- app/Filament/Resources/FilterResource/Pages/CreateFilter.php
+++ app/Filament/Resources/FilterResource/Pages/CreateFilter.php
@@ -3,10 +3,22 @@
 namespace App\Filament\Resources\FilterResource\Pages;
 
 use App\Filament\Resources\FilterResource;
+use App\Models\Filter;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class CreateFilter extends CreateRecord
 {
     protected static string $resource = FilterResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $filter = new Filter($data);
+        $filter->user_id = Auth::id();
+        $filter->save();
+
+        return $filter;
+    }
 }
