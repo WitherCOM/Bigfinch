5349985a87951a17828883a71810da59ec1e7200
WitherCOM <nemeth.csaba.tibor@gmail.com>
2025-08-18 10:19:06 +0200

    Update to l12

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -6,7 +6,7 @@ use App\Enums\CurrencyPosition;
 use App\Enums\Direction;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
--- app/Models/Integration.php
+++ app/Models/Integration.php
@@ -7,7 +7,7 @@ use App\Models\Scopes\OwnerScope;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Concerns\HasVersion4Uuids as HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
