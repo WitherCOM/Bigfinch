50c086844ea0051f87fbb4803531d6c026ec30e8
csaba.nemeth <csaba.nemeth@seon.io>
2025-08-19 09:10:54 +0200

    Change /admin to /

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/gocardless/callback', [\App\Http\Controllers\GocardlessController::class,'callback'])->name('gocardless.callback');
