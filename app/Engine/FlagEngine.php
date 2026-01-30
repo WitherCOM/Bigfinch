541c4550366b592c70d80fb5d3e0e43cc01d2764
Németh Csaba Tibor <nemeth.csaba.tibor@gmail.com>
2026-01-30 14:04:28 +0100

    Reworked calculation

--- app/Engine/FlagEngine.php
+++ app/Engine/FlagEngine.php
@@ -4,10 +4,12 @@ namespace App\Engine;
 
 use App\Enums\Direction;
 use App\Enums\Flag;
+use App\Models\Integration;
 use App\Models\Transaction;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class FlagEngine
 {
@@ -21,28 +23,49 @@ class FlagEngine
         return $transactions;
     }
 
-    const DATE_THRESHOLD = 300;
-    const VALUE_THRESHOLD = 5;
+    const INTERNAL_TO_KEYWORDS = [
+        'topup',
+        'top-up'
+    ];
+    const INTERNAL_FROM_KEYWORDS = [];
     /*
-     * Have to be run a date oriented array
      *
      */
     public static function detectInternalTransaction(Collection $transactions): Collection
     {
-        $dateSorted = $transactions->sortBy('date');
-        for ($index = 0; $index < count($dateSorted)-1; $index++) {
-            if (Carbon::parse($dateSorted[$index]->date)->diff(Carbon::parse($dateSorted[$index+1]->date))->seconds < self::DATE_THRESHOLD &&
-                abs($dateSorted[$index]->value*$dateSorted[$index]->currency->rate - $dateSorted[$index+1]->value*$dateSorted[$index+1]->currency->rate) < self::VALUE_THRESHOLD) {
-                    if ($dateSorted[$index]->direction == Direction::EXPENSE && $dateSorted[$index+1]->direction == Direction::INCOME) {
-                        $dateSorted[$index]->direction = Direction::INTERNAL_FROM;
-                        $dateSorted[$index+1]->direction = Direction::INTERNAL_TO;
-                    } else if ($dateSorted[$index+1]->direction == Direction::EXPENSE && $dateSorted[$index]->direction == Direction::INCOME) {
-                        $dateSorted[$index]->direction = Direction::INTERNAL_TO;
-                        $dateSorted[$index+1]->direction = Direction::INTERNAL_FROM;
+        $integrations = Integration::all();
+        for ($index = 0; $index < count($transactions); $index++) {
+            if (!is_null($transactions[$index]->integration_id)) {
+                $creditorName = Str::lower($transactions[$index]->open_banking_transaction['creditorName'] ?? "");
+                $debitorName = Str::lower($transactions[$index]->open_banking_transaction['debitorName'] ?? "");
+                $info = Str::lower(implode(" ", $transactions[$index]->open_banking_transaction["remittanceInformationUnstructuredArray"]));
+                if ($transactions[$index] == Direction::INCOME && Str::contains($info, self::INTERNAL_TO_KEYWORDS)) {
+                    $transactions[$index]->direction = Direction::INTERNAL_TO;
+                    continue;
+                }
+                if ($transactions[$index] == Direction::EXPENSE && Str::contains($info, self::INTERNAL_FROM_KEYWORDS)) {
+                    $transactions[$index]->direction = Direction::INTERNAL_FROM;
+                    continue;
+                }
+                $debitorCreditorIntegration = null;
+                foreach($integrations->where('user_id',$transactions[$index]->user_id) as $integration) {
+                    if (Str::contains($debitorName,$integration->institution_name) || Str::contains($creditorName,$integration->institution_name)) {
+                        $debitorCreditorIntegration = $integration;
+                        break;
                     }
+                }
+                if (!is_null($debitorCreditorIntegration) && $debitorCreditorIntegration->id != $transactions[$index]->integration_id) {
+                    if ($transactions[$index]->direction == Direction::EXPENSE) {
+                        $transactions[$index]->direction = Direction::INTERNAL_FROM;
+                        continue;
+                    } else if ($transactions[!$index]->direction == Direction::INCOME) {
+                        $transactions[$index]->direction = Direction::INTERNAL_TO;
+                        continue;
+                    }
+                }
             }
         }
-        return $dateSorted;
+        return $transactions;
     }
 
     const MATCH_INVESTORS = [
